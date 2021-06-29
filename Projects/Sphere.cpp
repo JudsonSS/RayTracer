1b718b39d67c805fd341cc10b0bec187f1c0a665
Judson Santiago <judson.santiago@gmail.com>
2021-06-29 02:53:21 -0300

    Atualizada a indentação

--- Projects/Sphere.cpp
+++ Projects/Sphere.cpp
@@ -1,7 +1,7 @@
 /**********************************************************************************
 // Sphere (Arquivo de Código Fonte)
 //
-// Criação:		29 Jun 2021
+// Criação:     29 Jun 2021
 // Atualização:	29 Jun 2021
 // Compilador:	Clang++ 12.0.5 / GNU g++ 9.3.0
 //
